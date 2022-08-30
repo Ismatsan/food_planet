34b94dd82e5b5aeb88b3695cc331cdfd770fefce
Ismatsan <ismat7300@gmail.com>
2022-08-30 16:28:04 +0600

    feat: added_elements_of_SecondBlok.js_&_SecondBlok.module.css

--- src/components/SecondBlok/SecondBlok.js
+++ src/components/SecondBlok/SecondBlok.js
@@ -15,6 +15,114 @@ const SecondBlok = () => {
                     </select>
                 </ul>
             </div>
+            <div className={style.pizza1}>
+                <div className={style.img1}>
+                </div>
+                <div className={style.title1}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc1}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost1}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza2}>
+                <div className={style.img2}>
+                </div>
+                <div className={style.title2}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc2}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost2}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza3}>
+                <div className={style.img3}>
+                </div>
+                <div className={style.title3}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc3}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost3}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza4}>
+                <div className={style.img4}>
+                </div>
+                <div className={style.title4}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc4}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost4}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza5}>
+                <div className={style.img5}>
+                </div>
+                <div className={style.title5}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc5}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost5}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza6}>
+                <div className={style.img6}>
+                </div>
+                <div className={style.title6}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc6}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost6}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza7}>
+                <div className={style.img7}>
+                </div>
+                <div className={style.title7}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc7}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost7}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.pizza8}>
+                <div className={style.img8}>
+                </div>
+                <div className={style.title8}>
+                    <b>Мексиканская</b>
+                </div>
+                <div className={style.desc8}>
+                    <p>Булка, котлета,сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус гриль, кетчуп, майонез</p>
+                </div>
+                <div className={style.cost8}>
+                    <p>200 сом</p>
+                </div>
+            </div>
+            <div className={style.btn}>
+                <button className={style.button}>Показать еще</button>
+            </div>
+
         </>
     );
 };
