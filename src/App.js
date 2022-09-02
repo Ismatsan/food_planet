d74b6279ec17f460fd096d3503db3430bf403426
Ismatsan <ismat7300@gmail.com>
2022-09-03 03:21:06 +0600

    feat: added_component_Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import Burger from "./components/FirstDispley/Burger";
 import style from './App.css';
 import Delivery from "./components/FirstDispley/Delivery/Delivery";
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import style from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={style.footer}>
+                <div className={style.menue}>
+                    <div className={style.logo}> </div>
+                    <div className={style.list}>
+                        <a className={style.mane} href="#" role="button">Главная</a>
+                        <select id="food" className={style.footerMenu}>
+                            <option value="">Меню</option>
+                            <option value="Pizzas">Пиццы</option>
+                            <option value="Cheeseburgers">Чизбургеры</option>
+                            <option value="Drinks">Напитки</option>
+                        </select>
+                        <a className={style.delivery} href="#" role="button">Доставка</a>
+                        <a className={style.contacts} href="#" role="button">Контакты</a>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 };
 
